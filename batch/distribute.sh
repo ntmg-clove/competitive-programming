ac6764ed0703b1bdab186437c1b02ce7c3f19dfe
nutmeg <goto.1fe.nut@gmail.com>
2022-02-20 02:18:28 +0900

    Add: テンプレ作成用batch

---
+++ batch/distribute.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+if [ -n "$1" ] ; then
+    cnt=$1
+else
+    echo "引数1に数字を指定してください"
+    exit 1
+fi
+
+cd ..
+echo 'templateの作成を行います。'
+
+for level in {a..d}
+do
+    cp -p ./python/template.py ./abc/${level}${1}.py
+done
