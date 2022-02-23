40d8d28f24751657787db9980b75c4957287f4f8
nutmeg <goto.1fe.nut@gmail.com>
2022-02-23 15:53:17 +0900

    Add: c++

---
+++ abc/c233.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+int main()
+{
+    int N, X;
+    vector<int> vec(N);
+    
+    cin >> N >> X;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> vec.at(i); 
+    }
+
+
+    
+    return 0;
+}
